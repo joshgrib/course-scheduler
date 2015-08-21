ea0b42e47e2b1fd103df6a8e22e7f8570322df45
joshgrib <jgribbon@stevens.edu>
2015-08-21 16:38:13 -0400

    Small documentation addition

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from app import app
 import scheduler  # file with all the functions
 import course_dict  # dictionary of all courses
+import forms  # login form ish
 
 
 @app.route('/')
@@ -13,7 +14,19 @@ def index():
 
 @app.route('/donate')
 def donate():
-    return render_template("donate.html")
+    return render_template("donate.html", title='Donate')
+
+
+@app.route('/courses')
+def courses():
+    these_courses = course_dict.getMeCourses()
+    sorted_courses = sorted(these_courses)
+    return render_template('courses.html', title='Courses', courses=these_courses, sorted_c=sorted_courses)
+
+
+@app.route('/scheduler')
+def scheduler():
+    pass
 
 
 @app.route('/sched/<someList>')
@@ -22,11 +35,4 @@ def scheduleMe(someList):
     courseList = someList.split(',')
     deezCombos = scheduler.schedule(courseList)
     # render it all with the template
-    return render_template("sched.html", combos=deezCombos)
-
-
-@app.route('/courses')
-def courses():
-    these_courses = course_dict.getMeCourses()
-    sorted_courses = sorted(these_courses)
-    return render_template('courses.html', courses=these_courses, sorted_c=sorted_courses)
+    return render_template("sched.html", title="Scheduler", combos=deezCombos)
