be4fb4d33d5465c11c40282d621e663dd2781427
joshgrib <jgribbon@stevens.edu>
2015-08-21 22:32:20 -0400

    Forms mostly working

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, request, redirect, make_response
 from app import app
+
 import scheduler  # file with all the functions
 import course_dict  # dictionary of all courses
+
 
 
 @app.route('/')
@@ -22,10 +24,34 @@ def courses():
     sorted_courses = sorted(these_courses)
     return render_template('courses.html', title='Courses', courses=these_courses, sorted_c=sorted_courses)
 
+@app.route('/how_many')
+def how_many():
+    return render_template("how_many.html")
 
-@app.route('/scheduler')
-def scheduler():
-    pass
+@app.route('/how_many', methods=['GET','POST'])
+def how_many_post():
+    course_amount = request.form['course_amount']
+    amount_of_courses = int(course_amount)
+    resp = make_response(render_template("schedule_entry.html", quantity = amount_of_courses))
+    resp.set_cookie('course_amount', str(amount_of_courses))
+    return resp
+
+@app.route('/schedules', methods=['GET','POST'])
+def my_form_post():
+    text_list = []
+    amount_of_courses = int(request.cookies.get('course_amount'))
+    for i in range(1, amount_of_courses+1):
+        form_num = 'text' + str(i)
+        text_list.append(request.form[form_num])
+    final_list = []
+    for text in text_list:
+        if not text == "":
+            final_list.append(text)
+    printout = ""
+    for course in final_list[:-1]:
+        printout += (str(course) + ',')
+    printout += str(final_list[-1])
+    return printout
 
 
 @app.route('/sched/<someList>')
