fa9a5d603cb4d5783bd25cdaaba9bdc1c284fd23
joshgrib <jgribbon@stevens.edu>
2015-08-22 03:45:59 -0400

    Added cookie to suppress donation popup

--- app/views.py
+++ app/views.py
@@ -5,12 +5,18 @@ import scheduler  # file with all the functions
 import course_dict  # dictionary of all courses
 
 
-
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Josh'}
-    return render_template("index.html", title='Home', user=user,)
+    visited = request.cookies.get('visited')
+    if visited == 'True':
+        resp = make_response(
+            render_template("index.html", title='Home', visted='True'))
+    else:
+        resp = make_response(
+            render_template("index.html", title='Home', visited='False'))
+        resp.set_cookie('visited', 'True', max_age=2592000)
+    return resp
 
 
 @app.route('/donate')
@@ -21,26 +27,31 @@ def donate():
 @app.route('/courses')
 def courses():
     these_courses = course_dict.getMeCourses()
+    # so I can list the courses in order
     sorted_courses = sorted(these_courses)
     return render_template('courses.html', title='Courses', courses=these_courses, sorted_c=sorted_courses)
+
 
 @app.route('/how_many')
 def how_many():
     return render_template("how_many.html", title='Couse amount entry')
 
-@app.route('/how_many', methods=['GET','POST'])
+
+@app.route('/how_many', methods=['GET', 'POST'])
 def how_many_post():
     course_amount = request.form['course_amount']
     amount_of_courses = int(course_amount)
-    resp = make_response(render_template("schedule_entry.html", quantity = amount_of_courses, title='Schedule Entry'))
-    resp.set_cookie('course_amount', str(amount_of_courses))
+    resp = make_response(render_template(
+        "schedule_entry.html", quantity=amount_of_courses, title='Schedule Entry'))
+    resp.set_cookie('course_amount', str(amount_of_courses), max_age=None)
     return resp
 
-@app.route('/schedules', methods=['GET','POST'])
+
+@app.route('/schedules', methods=['GET', 'POST'])
 def my_form_post():
     text_list = []
     amount_of_courses = int(request.cookies.get('course_amount'))
-    for i in range(1, amount_of_courses+1):
+    for i in range(1, amount_of_courses + 1):
         form_num = 'text' + str(i)
         text_list.append(request.form[form_num])
     final_list = []
@@ -53,6 +64,7 @@ def my_form_post():
     printout += str(final_list[-1])
     printout = printout.upper()
     my_url = '/sched/' + printout
+
     return redirect(my_url)
 
 
@@ -62,5 +74,5 @@ def scheduleMe(someList):
     courseList = someList.split(',')
     deezCombos = scheduler.schedule(courseList)
     # render it all with the template
-    combo_count =  str(len(deezCombos))
+    combo_count = str(len(deezCombos))
     return render_template("sched.html", title="Scheduler Results", combos=deezCombos, combo_amount=combo_count)
